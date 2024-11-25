5bd0c433f7750fbcaa349a470299ecd9132b83b0
rahulwd9 <95858230+rahulwd9@users.noreply.github.com>
2024-11-25 21:55:29 +0530

    Update B.java

--- B.java
+++ B.java
@@ -1,10 +1,10 @@
 public class B{
 
 public B(){
-  System.out.println("I am From B")
+  System.out.println("I am From B");
 }
   public void hi(){
-  System.out.println("I am From B void")
+  System.out.println("I am From B void");
   }
   
 }
